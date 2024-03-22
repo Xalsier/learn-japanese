1dd31c4865ea0149b9679f6245732b3d421fb336
Xalsier <xalsier@icloud.com>
2024-03-22 18:01:15 -0400

    Update fonts.js

---
+++ fonts.js
@@ -0,0 +1,12 @@
+const fonts = [
+    'Arial', 'Verdana', 'Courier', 'Times New Roman', 'Georgia',
+    'Comic Sans MS', 'Impact', 'Monaco', 'Lucida Console', 'Garamond',
+    'Brush Script MT', 'Cursive', 'Fantasy', 'Helvetica', 'Papyrus',
+    'Trebuchet MS', 'Lucida Sans Unicode', 'Gothic', 'Palatino Linotype', 'Century Gothic',
+    'Futura', 'Calibri', 'Arial Black', 'Cambria', 'Candara',
+    'Franklin Gothic Medium', 'Consolas', 'Corbel', 'Didot', 'Gill Sans',
+    'Optima', 'Perpetua', 'Rockwell', 'Tahoma', 'Baskerville',
+    'Copperplate', 'Franklin Gothic', 'Segoe UI', 'Serif', 'Times',
+    'Bookman Old Style', 'Bradley Hand', 'Courier New', 'Lucida Bright', 'Lucida Handwriting'
+];
+window.fonts = fonts;
